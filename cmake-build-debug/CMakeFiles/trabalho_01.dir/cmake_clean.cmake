5fe0451a1090ce0c24a768c3b96a4fb91a7fd099
João Matos <joao.deus@usp.br>
2022-05-16 13:19:50 -0300

    first

---
+++ cmake-build-debug/CMakeFiles/trabalho_01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trabalho_01.dir/main.c.o"
+  "trabalho_01"
+  "trabalho_01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/trabalho_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,43 @@
+//
+// Created by conta on 16/05/2022.
+//
+
+#include <stdio.h>
+#include <string.h>
+
+void tipo1(char nome_entrada[30], char nome_saida[30]);
+void tipo2(char nome_entrada[30], char nome_saida[30]);
+
+int main() {
+    char tipo[30];
+    char nome_entrada[30];
+    char nome_saida[30];
+    scanf("%s %s %s",tipo,nome_entrada,nome_saida);
+
+    if (strcmp(tipo, "tipo1") == 0){
+        tipo1( nome_entrada, nome_saida);
+    } else if (strcmp(tipo, "tipo2") == 0){
+        tipo2( nome_entrada, nome_saida);
+    } else {
+        printf("Tipo não está certo");
+    }
+    return 0;
+}
+
+void tipo1(char nome_entrada[30], char nome_saida[30]){
+    FILE entrada;
+    FILEsaida;
+
+
+    entrada= fopen(nome_entrada, "rb");
+    fclose(entrada);
+
+
+    saida= fopen(nome_saida, "w+b");
+    fclose(saida);
+
+}
+
+void tipo2(char nome_entrada[30], char nome_saida[30]){
+
+}
